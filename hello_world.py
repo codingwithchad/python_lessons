895368c60d960891ba41b23e33f748ce47faa835
Chad H <chadg1980@gmail.com>
2020-09-07 11:56:54 -0700

    added lesson 3

--- hello_world.py
+++ hello_world.py
@@ -2,16 +2,16 @@ x = 5
 y = 2
 myList = [10, 11, 12, 13, 14, 15]
 
-# print(x * y)  # Should print 10
-# print(x + y)  # Should print 7
-# print(x / y)  # Should print 2.5
-# print(x // y)  # Should print 2
-# print(x % 2)  # Should print 1
-# print("hello world")  # Should print hello world
-#
-# # should print the numbers 10 through 15
-# for number in myList:
-#     print(number)
+print(x * y)  # Should print 10
+print(x + y)  # Should print 7
+print(x / y)  # Should print 2.5
+print(x // y)  # Should print 2
+print(x % 2)  # Should print 1
+print("hello world")  # Should print hello world
+
+# should print the numbers 10 through 15
+for number in myList:
+    print(number)
 len(myList)
 print(myList[3])  # should print 13
 try:
