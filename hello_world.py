4c0f712c1cfbb1dd8a8a0230397f17105e640910
Chad H <chadg1980@gmail.com>
2020-10-01 18:30:15 -0700

    updated lesson 4

--- hello_world.py
+++ hello_world.py
@@ -4,20 +4,21 @@ myList = [10, 11, 12, 13, 14, 15]
 
 print(x * y)  # Should print 10
 print(x + y)  # Should print 7
+print(x - y)  # Should print 3
 print(x / y)  # Should print 2.5
 print(x // y)  # Should print 2
 print(x % 2)  # Should print 1
 print("hello world")  # Should print hello world
 
-# should print the numbers 10 through 15
-for number in myList:
-    print(number)
-len(myList)
-print(myList[3])  # should print 13
-try:
-    print(myList[6]) # Should throw error
-except IndexError as e:
-    print(e)
+# # should print the numbers 10 through 15
+# for number in myList:
+#     print(number)
+# len(myList)
+# print(myList[3])  # should print 13
+# try:
+#     print(myList[6]) # Should throw error
+# except IndexError as e:
+#     print(e)
 
 
 
